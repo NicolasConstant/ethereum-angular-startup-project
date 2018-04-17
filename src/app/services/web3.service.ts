5e18a7201fd076b17a4730d4c56a7dcad4e8a6f5
Nicolas Constant <github@nicolas-constant.com>
2018-04-16 22:10:07 -0400

    Added VS projects files

--- src/app/services/web3.service.ts
+++ src/app/services/web3.service.ts
@@ -1,41 +1,38 @@
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
-import { of } from "rxjs/observable/of";
-
 import * as Eth from "ethjs";
 declare var web3: any;
 
 @Injectable()
-export class Web3Service {  
-  constructor() {    
+export class Web3Service {
+  constructor() {
   }
 
   getEth(): Eth {
     let eth: Eth = null;
     try {
-      if(typeof web3 !== 'undefined') {
+      if (typeof web3 !== 'undefined') {
         eth = new Eth(web3.currentProvider);
       }
-    } catch(err){
+    } catch (err) {
       console.error(err);
     }
-    return eth;  
+    return eth;
   }
 
   isLocked(eth: Eth): Promise<boolean> {
     return eth.accounts()
-    .then((result: string[]) => {
-      if(result && result.length > 0){
-        return false;
-      }
-      return true;
-    })
-    .catch((error) => {
-      return true;
-    });
+      .then((result: string[]) => {
+        if (result && result.length > 0) {
+          return false;
+        }
+        return true;
+      })
+      .catch((error) => {
+        return true;
+      });
   }
 }
 
 export const WEB3_PROVIDERS: Array<any> = [
-{ provide: Web3Service, useClass: Web3Service }
+  { provide: Web3Service, useClass: Web3Service }
 ];
